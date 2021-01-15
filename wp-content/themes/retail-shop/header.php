a8c3ffacf59c9fa0e8ef21c38f3f8d7d71a47810
quyendv <quyendv@hblab.vn>
2021-01-16 00:25:39 +0700

    Add Header

--- wp-content/themes/retail-shop/header.php
+++ wp-content/themes/retail-shop/header.php
@@ -4,6 +4,22 @@
  * @package retail-shop
  * @since 1.0
  */
+
+global $wpdb;
+$args = array(
+    'taxonomy' => 'category',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'show_count' => 1,
+    'pad_counts' => 0,
+    'hierarchical' => 0,
+    'title_li' => '',
+    'hide_empty' => 1,
+);
+$cats = get_categories($args);
+//$moderation = $wpdb->get_col( $wpdb->prepare( "SELECT * FROM {$wpdb->ca} WHERE comment_approved = '0' LIMIT %d OFFSET %d", $limit, $start ) );
+
+
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
@@ -21,8 +37,116 @@
 	   $retail_shop_option = wp_parse_args(  get_option( 'ecommerce_star_option', array() ) , ecommerce_star_settings());  
 	}
 ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 </head>
 <body <?php body_class(); ?>>
+
+    <header class="custom-header">
+        <div class="header-content">
+            <div class="header-top">
+                <div class="header-top-search">
+                    <form class="top-search" action="http://localhost:6060/" method="get">
+                        <select class="d-none" name="product_cat">
+                            <option value="0" selected>All Categories</option>
+                        </select>
+
+                        <label class="screen-reader-text" for="woocommerce-product-search-field">Search for</label>
+                        <input type="search" name="s" id="" value="" placeholder="Bạn tìm kiếm gì ...">
+                        <button type="submit"><span class="fa icon fa-search"></span></button>
+                        <input type="hidden" name="post_type" value="product">
+                    </form>
+                </div>
+                <div class="header-top-nav">
+                    <a href="https://dinhduongbabau.net/">Trang chủ</a>
+                    <a href="https://dinhduongbabau.net/dinh-duong-ba-bau/">Giới thiệu</a>
+                    <a href="https://dinhduongbabau.net/lien-he/">Liên hệ</a>
+                    <div id="cart-wishlist-container">
+                        <table class="cart-wishlist-table">
+                            <tr>
+                                <td>
+                                    <?php if(class_exists('YITH_WCWL')): ?>
+                                        <div id="wishlist-top" class="wishlist-top">
+                                            <div class="wishlist-container">
+                                                <?php ecommerce_star_wishlist_count(); ?>
+                                            </div>
+                                        </div>
+                                    <?php endif; ?>
+                                </td>
+                                <td>
+                                    <div id="cart-top" class="cart-top">
+                                        <div class="cart-container">
+                                            <?php do_action( 'woocommerce_cart_top' ); ?>
+                                        </div>
+
+                                        <div id="popup-cart-wrap" class="widget woocommerce widget_shopping_cart "><?php woocommerce_mini_cart(); ?></div>
+
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="header-home">
+                <a href="<?php echo esc_url( home_url( '/' )) ?>"></a>
+            </div>
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <?php
+            $content = "";
+            foreach ($cats as $cat) {
+                if ($cat->category_parent == 0) {
+                    $content .= "<li><a href='" . esc_url( home_url( '/' )."?cat=".$cat->term_id ) . "'>$cat->name</a>";
+                    $hasCateChild = false;
+                    foreach ($cats as $catChild) {
+                        if ($catChild->category_parent == $cat->term_id) {
+                            if (!$hasCateChild) {
+                                $content .= "<ul>";
+                                $hasCateChild = true;
+                            }
+                            $content .= "<li><a href='" . esc_url( home_url( '/' )."?cat=".$catChild->term_id ) . "'>$catChild->name</a></li>";
+                        }
+                    }
+                    if ($hasCateChild) {
+                        $content .= "</ul></li>";
+                    } else {
+                        $content .= "</li>";
+                    }
+                }
+
+            }
+            echo $content;
+            ?>
+        </ul>
+    </nav>
+
+    <div class="banner-header owl-carousel owl-theme">
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="https://dinhduongbabau.net/wp-content/uploads/2019/11/canxi-cho-me.jpg" alt="">
+        </div>
+
+    </div>
 <?php
 	if ( function_exists( 'wp_body_open' ) ) {
 		wp_body_open();
@@ -48,25 +172,6 @@ $retail_shop_option['header_style'] = ecommerce_star_header_class();
 ?>
 <a class="skip-link screen-reader-text" href="#primary" ><?php esc_html_e( 'Skip to content', 'retail-shop' ); ?></a>
 <header id="masthead" class="<?php echo esc_attr(ecommerce_star_header_class()); ?> site-header" role="banner" >
-	
-	<?php get_template_part( 'template-parts/header/header', $retail_shop_option['mini_header_style'] );	?>
-
-	<?php	
-	if(is_front_page()  ) { 
-		get_template_part( 'sections/top', 'widgets' ); 
-	}	
-	?>
-
-	<div class="container">
-		<?php
-		if($retail_shop_option['header_woocommerce'] && class_exists( 'WooCommerce' )){
-			//ecommerce_star_woocommerce_header();
-			ecommerce_star_woocommerce_header();
-		} else {
-			ecommerce_star_default_header(); 
-		}
-		?>
-	</div><!-- .container -->
 	
 		<!--display menu bar full row when header options, woocommerce layout with search--> 
 		<?php 
