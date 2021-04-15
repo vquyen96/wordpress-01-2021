a2ba35347b4c93a1b1c4d7133ea9afaee090e623
qdv <quyendv@hblab.vn>
2021-04-15 21:52:49 +0700

    Add page manages video

--- wp-content/plugins/hunmend-customs/video.php
+++ wp-content/plugins/hunmend-customs/video.php
@@ -1,57 +1,56 @@
 <?php
 global $wpdb;
+$videoId = ( isset( $_GET['id'] ) ? (int) sanitize_key( $_GET['id'] ) : 0 );
 
-if (isset($_POST['do']) && $bannerId == 0) {
-    if ($bannerUrl == null) {
-        $alertError = "Hình ảnh không được để trống";
-    }
-    $dataBanner = [
-        'title' => $_POST['title'],
-        'value' => $bannerUrl,
-        'links' => $_POST['links'],
-        'sort'  => 1,
-        'type'  => $_POST['type'],
-        'status'=> 1
-    ];
-    $bannerResult = $wpdb->insert(
-        $wpdb->hunmend_banners,
-        $dataBanner,
-        [
-            '%s',
-            '%s',
-            '%s',
-            '%d',
-            '%d',
-            '%d',
-        ]
-    );
-} else if (isset($_POST['do']) && $bannerId != 0) {
-    $dataBanner = [
-        'title' => $_POST['title'],
-        'links' => $_POST['links'],
-        'sort'  => 1,
-        'type'  => $_POST['type'],
-        'status'=> 1
-    ];
-    $dataType = [
-        '%s',
-        '%s',
-        '%d',
-        '%d',
-        '%d',
-    ];
-    if ($bannerUrl != null) {
-        $dataBanner['value'] = $bannerUrl;
-        $dataType[] = '%s';
-    }
+if ($videoId != 0) {
+    $postVideo = get_post( $videoId );
+}
 
-    $bannerResult = $wpdb->update(
-        $wpdb->hunmend_banners,
-        $dataBanner,
-        ['id' => $bannerId],
-        $dataType,
-        ['%d']
-    );
+if (isset($_POST['do']) && $videoId != 0) {
+    $hunmendVideo =  $wpdb->get_row("SELECT * FROM $wpdb->hunmend_videos WHERE status = 1 AND post_id = ".$_POST['post_id']);
+    if ($hunmendVideo == null) {
+        $videoResult = $wpdb->insert(
+            $wpdb->hunmend_videos,
+            [
+                'post_id' => $_POST['post_id'],
+                'youtube_id' => $_POST['links'],
+                'post_title' => $postVideo->post_title,
+                'sort' => 1,
+                'view_count' => 0,
+                'status' => 1
+            ],
+            [
+                '%s',
+                '%s',
+                '%s',
+                '%d',
+                '%d',
+                '%d',
+            ]
+        );
+    } else {
+        $videoResult = $wpdb->update(
+            $wpdb->hunmend_videos,
+            [
+                'post_id' => $_POST['post_id'],
+                'youtube_id' => $_POST['links'],
+                'post_title' => $postVideo->post_title,
+                'sort' => 1,
+                'view_count' => 0,
+                'status' => 1
+            ],
+            ['id' => $videoId],
+            [
+                '%s',
+                '%s',
+                '%s',
+                '%d',
+                '%d',
+                '%d',
+            ],
+            ['%d']
+        );
+    }
 }
 
 $postVideos = get_posts([
@@ -64,6 +63,17 @@ $postVideos = get_posts([
 $query = "SELECT * FROM $wpdb->hunmend_videos ORDER BY id DESC";
 $listVid =  $wpdb->get_results( $query);
 
+$listVideos = '(';
+foreach ($postVideos as $video) {
+    $listVideos .= $video->ID.',';
+}
+$listVideos .= '0)';
+
+$hunmendVideos =  $wpdb->get_results("SELECT * FROM $wpdb->hunmend_videos WHERE status = 1 AND post_id in ".$listVideos."  ORDER BY id DESC");
+$listVideoIds = [];
+foreach ($hunmendVideos as $hmVideo) {
+    $listVideoIds[$hmVideo->post_id] = $hmVideo->youtube_id;
+}
 
 ?>
 
@@ -71,6 +81,34 @@ $listVid =  $wpdb->get_results( $query);
     <div class="question-title d-flex">
         <h1><?php _e('Quản lý video', 'hunmend-customs') ; ?></h1>
     </div>
+    <?php if ($videoId != 0) { ?>
+    <div class="banner-add-new">
+        <form action="" method="post" enctype="multipart/form-data">
+            <h3><?php _e('Chỉnh sửa video', 'hunmend-customs'); ?></h3>
+            <table class="form-table">
+                <tr>
+                    <th width="20%" scope="row" valign="top"><?php _e('Tên', 'hunmend-customs') ?></th>
+                    <td width="80%">
+                        <input type="text" size="70" name="title" value="<?php echo $postVideo->post_title?>" disabled/>
+                        <input type="hidden" size="70" name="post_id" value="<?php echo $postVideo->ID?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <th width="20%" scope="row" valign="top"><?php _e('Link', 'hunmend-customs') ?></th>
+                    <td width="80%">
+                        <input type="text" size="70" name="links" value="<?php echo isset($listVideoIds[$postVideo->ID]) ? $listVideoIds[$postVideo->ID] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <th width="20%" scope="row" valign="top"></th>
+                    <td width="80%">
+                        <input type="submit" name="do" value="<?php _e('Cập Nhật', 'hunmend-customs') ; ?>"  class="button-primary" />
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    <?php }?>
     <div class="question-manages">
         <h3><?php _e('Danh sách video', 'hunmend-customs'); ?></h3>
         <table class="widefat">
@@ -83,13 +121,13 @@ $listVid =  $wpdb->get_results( $query);
             </tr>
             </thead>
             <tbody id="manage_polls">
-            <?php foreach($postVideos as $index => $postVid) { ?>
+            <?php foreach($postVideos as $index => $hunmendVid) { ?>
             <tr>
                 <td><?php echo $index+1?></td>
-                <td><?php echo $postVid->post_title?></td>
-                <td><?php echo $postVid->post_title?></td>
+                <td><?php echo $hunmendVid->post_title?></td>
+                <td><?php echo $hunmendVid->post_title?></td>
                 <td>
-                    <button class="edit-vid"><?php _e('Sửa vid', 'hunmend-customs') ?></button>
+                    <a href="admin.php?page=hunmend-customs%2Fvideo.php&amp;id=<?php echo $hunmendVid->ID?>" class="edit-vid"><?php _e('Sửa vid', 'hunmend-customs') ?></a>
                     <button class="detail-post"><?php _e('Chi tiết', 'hunmend-customs') ?></button>
                 </td>
             </tr>
